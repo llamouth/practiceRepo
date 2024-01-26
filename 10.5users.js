b7a40001c2860a144ec047959923c04ea879a470
llamouth <150949923+llamouth@users.noreply.github.com>
2024-01-26 14:05:57 -0500

    update questions

---
+++ 10.5users.js
@@ -0,0 +1,239 @@
+const users = [
+  {
+    firstName: "josh",
+    lastName: "nelson",
+    age: "29",
+    eyeColor: "brown",
+    heightInCm: "172",
+    wearsGlasses: false,
+    pronouns: 'he/him'
+  },
+  {
+    firstName: "nadia",
+    lastName: "matos",
+    age: "32",
+    eyeColor: "brown",
+    heightInCm: "155",
+    wearsGlasses: true,
+    pronouns: 'she/her'
+  },
+  {
+    firstName: "alexandra",
+    lastName: "perez",
+    age: "23",
+    eyeColor: "brown",
+    heightInCm: "160",
+    wearsGlasses: true,
+    pronouns: 'she/her'
+  },
+  {
+    firstName: "Justin",
+    lastName: "Overton",
+    age: "30",
+    eyeColor: "brown",
+    heightInCm: "177",
+    wearsGlasses: true,
+    pronouns: 'he/him'
+  },
+  {
+    firstName: "markedia",
+    lastName: "hinds",
+    age: "88",
+    eyeColor: "brown",
+    heightInCm: "163",
+    wearsGlasses: true,
+    pronouns: 'she/her'
+  },
+  {
+    firstName: "Larry",
+    lastName: "Lamouth",
+    age: "28",
+    eyeColor: "brown",
+    heightInCm: "182.88",
+    wearsGlasses: false,
+    pronouns: 'he/him'
+  },
+  {
+    firstName: 'Vianel',
+    lastName: 'De La Cruz',
+    age: '34',
+    eyeColor: 'brown',
+    heightInCm: '181',
+    wearsGlasses: false,
+    pronouns: 'he/him'  
+  },
+  {
+    firstName: 'Tasmyn',
+    lastName: 'Craig',
+    age: '45',
+    eyeColor: 'brown',
+    heightInCm: '163.4',
+    wearsGlasses: true,
+    pronouns: 'she/her'
+  },
+  {
+    firstName:"Ayotunde",
+    lastName: "Akinkunmi",
+    age: "50",
+    eyeColor: "Brown",
+    heightInCm: "176",
+    wearsGlasses: false,
+    pronouns: "he/him"
+  },
+  {
+    firstName: "Sadya",
+    lastName: "Andrickson",
+    age: "28",
+    eyeColor: "brown",
+    heightInCm: "162",
+    wearGlasses: true,
+    pronouns: "she/her"
+  },
+  {
+    firstName: "rizel",
+    lastName: "enad",
+    age: "48",
+    eyeColor: "brown",
+    heightInCm: "162",
+    wearsGlasses: true,
+    pronouns: "she/her"
+  },
+  {
+    firstName: "Nicole",
+    lastName: "Marin",
+    age: "21",
+    eyeColor: "brown",
+    heightInCm: "160",
+    wearsGlasses: true,
+    pronouns: "she/her",
+  },
+  {
+    firstName : 'Moinul',
+    lastName : 'Khan',
+    age: '18',
+    eyeColor : 'brown',
+    heightInCm : '178',
+    wearsGlasses: true,
+    pronouns : 'him'
+  },
+  {
+    firstName: "Brendan",
+    lastName: "O\'Brien",
+    age: 42,
+    eyeColor: "blue",
+    heightInCm: "177.8",
+    wearsGlasses: true,
+    pronouns: 'he/him',
+  },
+  {
+    firstName: "Manoel",
+    lastName: "Teixeira",
+    age: "34",
+    eyeColor: "dark brown",
+    heightInCm: "187.5",
+    wearsGlasses: true,
+    pronouns: "he/him"
+  },
+  {
+    firstName: "Kenny",
+    lastName: "Second",
+    age: "29",
+    eyeColor: "brown",
+    heightInCm: "177.8",
+    wearsGlasses: false,
+    pronouns: "he/him",
+  },
+  {
+    firstName: "Jean",
+    lastName: "Moncayo",
+    age: "37",
+    eyeColor: "brown",
+    heightInCm: "172.72",
+    wearsGlasses: false,
+    pronouns: "he/him",
+  },
+  {
+    firstName: "Jacob",
+    lastName: "Lebowitz",
+    age: "23",
+    eyeColor: "Hazel",
+    heightInCm: "167",
+    wearsGlasses: false,
+    pronouns: "any/all",
+  },
+  {
+    firstName: "E-Vadnie",
+    lastName: "Lewis",
+    age: "30",
+    eyeColor: "brown",
+    heightInCm: "165",
+    wearsGlasses: true,
+    pronouns: "she/they"
+  },
+  { 
+    firstName: "David", 
+    lastName: "Cruz", 
+    age: "38", 
+    eyeColor: "brown", 
+    heightInCm: "172.72", 
+    wearsGlasses: true, 
+    pronouns: "He/Him" 
+  },
+  {
+    firstName: "Chris",
+    lastName: "Sanchez",
+    age: "33",
+    eyeColor: "brown",
+    heightInCm: "173.72",
+    wearsGlasses: true,
+    pronouns: 'he/him'
+    },
+    {
+    firstName: 'Camilo',
+    lastName: 'Paez',
+    age: '37',
+    eyeColor: 'Brown',
+    heightInCm: '175',
+    wearGlasses: true,
+    pronouns: 'he/him'
+  },
+  {
+    firstName: "Darrion",
+    lastName: "Crowder",
+    age: "19",
+    eyeColor: "brown",
+    heightInCm: "182.88",
+    wearsGlasses: true,
+  },
+  {
+    firstName: 'Aaron',
+    lastName: 'Constant',
+    age: '30',
+    eyeColor: 'Brown',
+    heightInCm: "172.72",
+    wearsGlasses: false,
+    pronouns: 'he/him'
+  },
+  {
+    firstName: "Diamond",
+    lastName: "Brown",
+    age: "22",
+    eyeColor: "brown",
+    heightInCm: "172.",
+    wearsGlasses: false,
+    pronouns: "she/her",
+  },
+  {
+    firstName: 'Warren',
+    lastName: 'anderson',
+    age: 'infinity',
+    eyeColor: 'Brown',
+    heightInCm: '74',
+    wearsGlasses: false,
+    pronouns: 'he/him'
+  }
+  
+  
+  ];
+  
+  module.exports = users;
