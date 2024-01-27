b74f2250de89481804e4ced9e38f2419b0c64b96
llamouth <150949923+llamouth@users.noreply.github.com>
2024-01-27 17:17:37 -0500

    update readme.md

---
+++ dataPractice2.js
@@ -0,0 +1,100 @@
+const users = require("./10.5users")
+  
+/** Problem 1:
+   Write a function that takes in an array of users and returns the oldest user
+   * @param {Object[]} users - An array of users.
+   * @returns {number} The age of the oldest user.
+*/
+
+const whoIsTheOldest = (users) => {
+   
+}
+
+console.log(whoIsTheOldest(users))
+
+/** Problem 2:
+   Write a function that takes in an array of users and returns who wears glasses
+   * @param {Object[]} users - An array of users.
+   * @returns {Object[]} Array of users who wears glasses
+*/
+
+const whoWearsGlasses = (users) => {
+
+}
+
+console.log(whoWearsGlasses(users))
+
+/** Problem 3:
+   Write a function that takes in an array of users and returns who is tallest user
+   * @param {Object[]} users - An array of users.
+   * @returns {String} The tallest user
+*/
+
+const findTheUserWhoIsTheTallest = (users) => {
+
+}
+
+console.log(`The tallest is ${findTheUserWhoIsTheTallest(users)}`)
+
+/** Problem 4:
+   Write a function that takes in an array of users and returns the shortest user
+   * @param {Object[]} users - An array of users.
+   * @returns {String} The shortest user full name & height
+*/
+
+const findTheUserWhoIsTheShortest = (users) => {
+
+}
+
+console.log(`The shortest is ${findTheUserWhoIsTheShortest(users)}`)
+
+/** Problem 5:
+   Write a function that takes in an array of users and returns the users over 30
+   * @param {Object[]} users - An array of users.
+   * @returns {Object[]} An array of users over 30
+*/
+
+const gettingUsersOver30 = (users) => {
+
+}
+
+console.log(gettingUsersOver30(users))
+
+/** Problem 6:
+   Write a function that takes in an array of users and returns a count of how many last name begins first letter E.X.{A: 3, B: 2, C: 1, F: 2}
+   * @param {Object[]} users - An array of users.
+   * @returns {Object{}} An object containing the count of users begininng with the correct last name
+*/
+
+const howManyLastNameBginsWithObject = (users) => {
+
+}
+
+console.log(howManyLastNameBginsWithObject(users))
+
+/** Problem 7:
+*Given users, return am array of each of the users full name capitalized or if there is a target value return the target if found in the user data if not found return "User not found".
+*@param {Object[]} = users array
+*@return {Object[], String} = new aray with user's full name || Target if target value is present
+*/
+
+const getUserFullName = (users, target) => {
+
+}
+
+console.log(getUserFullName(users))
+console.log(getUserFullName(users, "Habibi"))
+console.log(getUserFullName(users, "larry lamouth"))
+console.log(getUserFullName(users, "josh nelson"))
+console.log(getUserFullName(users, "Tony ezekiel"))
+
+
+module.exports = {
+   whoIsTheOldest,
+   whoWearsGlasses,
+   findTheUserWhoIsTheTallest,
+   findTheUserWhoIsTheShortest,
+   gettingUsersOver30,
+   howManyLastNameBginsWithObject,
+   getUserFullName
+};;
